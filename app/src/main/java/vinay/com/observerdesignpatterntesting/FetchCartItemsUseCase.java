c5f45a04407a07e94fcf8bdb0e726900b6565d5d
Vinay Mathew John <vinay.john@litmus7.com>
2020-05-05 10:13:39 +0530

    Adding Test Cases using do answer, Argument Captor etc

--- app/src/main/java/vinay/com/observerdesignpatterntesting/FetchCartItemsUseCase.java
+++ app/src/main/java/vinay/com/observerdesignpatterntesting/FetchCartItemsUseCase.java
@@ -1,6 +1,40 @@
 package vinay.com.observerdesignpatterntesting;
 
+import java.util.List;
+
+import vinay.com.observerdesignpatterntesting.cart.CartItem;
+import vinay.com.observerdesignpatterntesting.networking.CartItemSchema;
+import vinay.com.observerdesignpatterntesting.networking.GetCartItemsHttpEndpoint;
+
 public class FetchCartItemsUseCase {
 
+    private final GetCartItemsHttpEndpoint getCartItemsHttpEndpoint;
 
+    public FetchCartItemsUseCase(GetCartItemsHttpEndpoint getCartItemsHttpEndpoint) {
+        this.getCartItemsHttpEndpoint = getCartItemsHttpEndpoint;
+    }
+
+
+    public void fetchCartItemsAndNotify(int limit) {
+        getCartItemsHttpEndpoint.getCartItems(limit, new GetCartItemsHttpEndpoint.Callback() {
+            @Override
+            public void onGetCartItemsSucceeded(List<CartItemSchema> cartItems) {
+
+            }
+
+            @Override
+            public void onGetCartItemsFailed(GetCartItemsHttpEndpoint.FailReason failReason) {
+
+            }
+        });
+    }
+
+
+    public void registerListener(Listener mListenerMock1) {
+
+    }
+
+    public interface Listener {
+        void onCartItemsFetched(List<CartItem> capture);
+    }
 }
--- app/src/test/java/vinay/com/observerdesignpatterntesting/FetchCartItemsUseCaseTest.java
+++ app/src/test/java/vinay/com/observerdesignpatterntesting/FetchCartItemsUseCaseTest.java
@@ -3,27 +3,95 @@ package vinay.com.observerdesignpatterntesting;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vinay.com.observerdesignpatterntesting.cart.CartItem;
+import vinay.com.observerdesignpatterntesting.networking.CartItemSchema;
+import vinay.com.observerdesignpatterntesting.networking.GetCartItemsHttpEndpoint;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FetchCartItemsUseCaseTest {
 
+    public static final int LIMIT = 10;
+    public static final String ID = "id";
+    public static final String TITLE = "title";
+    public static final String DESCRIPTION = "description";
+    public static final int PRICE = 10;
     FetchCartItemsUseCase mFetchCartItemsUseCase;
+
+    @Mock
+    GetCartItemsHttpEndpoint getCartItemsHttpEndpointMock;
+
+    @Mock
+    FetchCartItemsUseCase.Listener mListenerMock1;
+
+    @Mock
+    FetchCartItemsUseCase.Listener mListenerMock2;
+
+    @Captor
+    ArgumentCaptor<List<CartItem>> listArgumentCaptor;
 
     @Before
     public void setUp() throws Exception {
-        mFetchCartItemsUseCase = new FetchCartItemsUseCase();
+        mFetchCartItemsUseCase = new FetchCartItemsUseCase(getCartItemsHttpEndpointMock);
+        success();
     }
 
     //correct limit passed to the endpoint
     @Test
-    public void fetchCartItems_correctLimitsPassedToEndPoint(){
-
+    public void fetchCartItems_correctLimitsPassedToEndPoint() {
+        //Arrange
+        ArgumentCaptor<Integer> acInt = ArgumentCaptor.forClass(Integer.class);
+        //Act
+        mFetchCartItemsUseCase.fetchCartItemsAndNotify(LIMIT);
+        //Assert
+        verify(getCartItemsHttpEndpointMock).getCartItems(acInt.capture(), any(GetCartItemsHttpEndpoint.Callback.class));
+        assertThat(acInt.getValue(), is(LIMIT));
     }
 
     //success - all observers notified with correct data
+    @Test
+    public void fetchCartItems_success_observersNotifiedwithCorrectData() {
+        //Arrange
+        //Act
+        mFetchCartItemsUseCase.registerListener(mListenerMock1);
+        mFetchCartItemsUseCase.registerListener(mListenerMock2);
+        mFetchCartItemsUseCase.fetchCartItemsAndNotify(LIMIT);
+
+        //Assert
+        verify(mListenerMock1).onCartItemsFetched(listArgumentCaptor.capture());
+        verify(mListenerMock2).onCartItemsFetched(listArgumentCaptor.capture());
+
+
+        List<List<CartItem>> captures = listArgumentCaptor.getAllValues();
+        List<CartItem> capture1 = captures.get(0);
+        List<CartItem> capture2 = captures.get(1);
+        assertThat(capture1, is(getCartItems()));
+        assertThat(capture2, is(getCartItems()));
+
+    }
+
+    private List<CartItem> getCartItems() {
+        List<CartItem> cartItems = new ArrayList<>();
+        cartItems.add(new CartItem(ID, TITLE, DESCRIPTION, PRICE));
+        return cartItems;
+    }
 
     //success - unsubscribe observers not notified
 
@@ -32,5 +100,21 @@ public class FetchCartItemsUseCaseTest {
     //network error - observers notified of failure
 
 
+    private void success() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object[] args = invocation.getArguments();
+                GetCartItemsHttpEndpoint.Callback callback = (GetCartItemsHttpEndpoint.Callback) args[1];
+                callback.onGetCartItemsSucceeded(getCartItemSchemes());
+                return null;
+            }
+        }).when(getCartItemsHttpEndpointMock).getCartItems(anyInt(), any(GetCartItemsHttpEndpoint.Callback.class));
+    }
 
+    private List<CartItemSchema> getCartItemSchemes() {
+        List<CartItemSchema> schemas = new ArrayList<>();
+        schemas.add(new CartItemSchema(ID, TITLE, DESCRIPTION, PRICE));
+        return schemas;
+    }
 }
